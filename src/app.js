3f36fb239c71abf0f431e938a3a0c4114b35c434
Oliver Simkovsky <53585119+fluffbutt1@users.noreply.github.com>
2026-01-10 01:04:06 +0000

    started java script functionality

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const p = document.getElementById("excuse");
+  console.log(p.textContent);
+
+  let testArray = ["a","b","c"];
+
+  function getRandomIndex(array){
+    const length = array.length;
+    const result = Math.floor(Math.random() * length);
+    return result;
+  }
+
+  console.log(testArray[getRandomIndex(testArray)]);
+  p.textContent = testArray[getRandomIndex(testArray)];
 };
