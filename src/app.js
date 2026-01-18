d2941a6712ead863a5c7733e8664ef75d54fa3d6
Oliver Simkovsky <53585119+fluffbutt1@users.noreply.github.com>
2026-01-18 20:52:20 +0000

    added array that contains 'when' strings

--- src/app.js
+++ src/app.js
@@ -5,36 +5,47 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
   const documentID = document.getElementById("excuse");
 
-  const testArray = ["Chicken tenders","Oliver","Goku", "Naruto"];
+  const testArray = ["Chicken tenders", "Oliver", "Goku", "Naruto"];
   const secondArray = ["kamehameha'd", "broke", "ate", "stole"];
   const thirdArray = ["my homework", "the car", "my will to live"];
+  const fourthArray = ["while I was sleeping",
+    "while I was training to be Hokage",
+    "while I was training with Picollo",
+    "while I was taking a bathroom break"];
 
-  function getRandomIndex(array){
+  function getRandomIndex(array) {
     const length = array.length;
     const result = Math.floor(Math.random() * length);
 
     return result;
 
   }
-  function getSecondRandomIndex(secondArray){
+  function getSecondRandomIndex(secondArray) {
     const secondLength = secondArray.length;
     const secondResult = Math.floor(Math.random() * secondLength);
 
     return secondResult;
   }
 
-  function getThirdRandomIndex(thirdArray){
+  function getThirdRandomIndex(thirdArray) {
     const thirdLength = thirdArray.length;
     const thirdResult = Math.floor(Math.random() * thirdLength);
 
     return thirdResult;
   }
 
-  let finalResult = testArray[getRandomIndex(testArray)] + " " + secondArray[getSecondRandomIndex(secondArray)] + " " + thirdArray[getThirdRandomIndex(thirdArray)];
+  function getFourthRandomIndex(fourthArray) {
+    const fourthLength = fourthArray.length;
+    const fourthResult = Math.floor(Math.random() * fourthLength);
+
+    return fourthResult;
+  }
+
+  let finalResult = testArray[getRandomIndex(testArray)] + " " + secondArray[getSecondRandomIndex(secondArray)] + " " + thirdArray[getThirdRandomIndex(thirdArray)] + " " + fourthArray[getFourthRandomIndex(fourthArray)];
   console.log(finalResult);
   documentID.textContent = finalResult;
 };
